ae3ed20f7ff160e4fff7d91e8d1a28d3b576cbd1
Andres Rojas <andresrojas12498706@gmail.com>
2021-12-01 18:12:56 -0500

    docs agregado

---
+++ docs/precache-manifest.ee903966a819b40b2cdf7f7faead2fe9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "133b06990757c078cf7105aa9cdacc4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9649693bf1b0bf211ba8",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9649693bf1b0bf211ba8",
+    "url": "/static/js/main.9508f838.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
